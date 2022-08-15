9f955a3ba2575de6a2b7c3f5cc154b4d05229b9f
MaryannChidimma <okonkwo20chidimma@gmail.com>
2022-08-15 07:32:03 +0100

    saving plan model

---
+++ src/models/savingPlanModel.ts
@@ -0,0 +1,45 @@
+const { Sequelize, DataTypes} = require('sequelize');
+
+
+  const SavingPlanAttributes = 
+  {
+    title: {
+      type: DataTypes.STRING,
+    },
+    numberOfBuddies: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    savingFrequency: {
+      type: Sequelize.ENUM("Daily", "Weekly", "Monthly"),
+      allowNull: false,
+    },
+    specifyTarget: {
+        type: Sequelize.ENUM("Yes", "No"),
+    },
+
+    yearlyTarget: {
+        type: DataTypes.INTEGER,
+    },
+    savingmethod: {
+        type: Sequelize.ENUM("Automatic", "Manual"),
+    },
+    relationshipWithBuddies: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    invitedBuddies: {
+        type: Sequelize.ARRAY(DataTypes.STRING),
+    },
+    startDate: {
+        type: Sequelize.DATE,
+        defaultValue: false,
+    },
+    enddate: {
+        type: Sequelize.DATE,
+        defaultValue: false,
+    },
+  };
+  
+export default SavingPlanAttributes
