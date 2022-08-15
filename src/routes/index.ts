2b81b2a611fa695b5d9139921d20ad482a97af60
MaryannChidimma <okonkwo20chidimma@gmail.com>
2022-08-15 11:39:02 +0100

    modified

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import userRouter from "./userRoute";
 import savingPlanRouter from "./savingPlanRoute";
+import inviteRouter from "./invitationRoute"
 
 const router = express.Router();
 
 function rootRouter() {
   router.use("/user", userRouter());
   router.use("/saving-plan", savingPlanRouter())
+  router.use("/invite", inviteRouter())
   
   return router;
 }
